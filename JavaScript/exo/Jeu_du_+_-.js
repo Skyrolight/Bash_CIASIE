81378aab9897de61d771a6a127780b57d75fbc88
skyrolight <skyrolight@gmail.com>
2022-10-19 21:28:29 +0200

    Exercice 2 fini

--- JavaScript/exo/Jeu_du_+_-.js
+++ JavaScript/exo/Jeu_du_+_-.js
@@ -2,16 +2,19 @@ function getRandomNumber() {
     return Math.floor(Math.random() * (99 - 1 + 1) + 1)
 }
 
-function getHighscore(id) {
-    var test = JSON.parse(localStorage.getItem("history"));
-    return test;
+function getLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+function setHistory(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
 }
 
 function game(randomNumber, tryNumber) {
-    // console.log(getHighscore());
+    let key = (Object.keys(localStorage).length) - 1;
+    const highscore = getLocalStorage('highscore');
     tryNumber++;
     let userValue = prompt("Entrez une valeur entre 1 et 99:");
-
     if (!(userValue < 1 || userValue > 99)) {
         if (randomNumber < userValue) {
             console.log("le nombre à trouver est plus petit")
@@ -22,10 +25,20 @@ function game(randomNumber, tryNumber) {
         if (userValue != randomNumber) {
             game(randomNumber, tryNumber);
         } else {
-            console.log('Bravo!')
-            console.log(`Vous avez trouvé le nombre en: ${tryNumber} coup\(s)`)
+            console.log('Bravo!');
+            console.log(`Vous avez trouvé le nombre en: ${tryNumber} coup\(s)`);
+
             let userName = prompt("Entrez votre pseudo :");
-            localStorage.setItem('history', [`${userName}, ${tryNumber}`]);
+            const data = {
+                userName,
+                tryNumber
+            };
+
+            setHistory(`userName${key}`, data)
+            if (tryNumber < highscore) {
+                localStorage.setItem('highscore', tryNumber);
+                console.log("Vous détenez le recors !");
+            }
         }
 
     } else {
@@ -36,10 +49,13 @@ function game(randomNumber, tryNumber) {
 function initGame() {
     const randomNumber = 12;
     let tryNumber = 0;
-    const datas = ['', 999];
+    let highscore = getLocalStorage('highscore');
 
-    if (localStorage.getItem('history') === null) {
-        localStorage.setItem('history', JSON.stringify(datas));
+    if (highscore === null || highscore == "999" ) {
+        localStorage.setItem('highscore', 999);
+        console.log("Il n'y a pas de high score")
+    } else {
+        console.log(`Le high score est de : ${highscore}`);
     }
 
     game(randomNumber, tryNumber);
