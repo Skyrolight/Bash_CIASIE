a1e2fd933c09f6bd0efc045a03b3dbab4c5fe8c5
Alban-Ktz <alban.ktz@gmail.com>
2022-10-19 13:57:56 +0200

    finish exercice 1 html/css

--- JavaScript/exo/Jeu_du_+_-.js
+++ JavaScript/exo/Jeu_du_+_-.js
@@ -1,12 +1,21 @@
-let randomNumber = 12
+let randomNumber = 12 //Math.floor(Math.random() * (99 - 1 + 1) + 1)
+let response = document.getElementById("response");
+let tryNumber = 0;
 
 function game() {
     let userInput = document.getElementById("userNumber").value;
 
-    if (randomNumber < userInput) {
-        console.log("le nombre à trouver est plus petit")
+    if (!(userInput > 99 || userInput < 1)) {
+        if (randomNumber < userInput) {
+            response.innerHTML = "le nombre a trouver est plus petit"
+            tryNumber++
+        } else if (randomNumber > userInput ) {
+            response.innerHTML = "le nombre a trouver est plus grand"
+            tryNumber++
+        } else {
+            response.innerHTML = `Félicitations, vous avez trouvé le nombre mystère en ${tryNumber} tentatives !`
+        }
     } else {
-        console.log("le nombre à trouver est plus grand")
-    } 
-
+        response.innerHTML = "Veuillez saisir un nombre entre 1 et 99"
+    }
 }
